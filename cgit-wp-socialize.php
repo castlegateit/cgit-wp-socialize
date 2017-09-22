faf65a90d8bd46a0ec334fed0ffa01fc02a5dca6
John Hughes <john@castlegateit.co.uk>
2017-09-22 13:37:23 +0100

    Fix missing constant

--- cgit-wp-socialize.php
+++ cgit-wp-socialize.php
@@ -5,7 +5,7 @@
 Plugin Name: Castlegate IT WP Socialize
 Plugin URI: http://github.com/castlegateit/cgit-wp-socialize
 Description: Simple social network sharing URLs, links, and widgets.
-Version: 2.1
+Version: 2.2
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
@@ -15,6 +15,8 @@ License: MIT
 if (!defined('ABSPATH')) {
     wp_die('Access denied');
 }
+
+define('CGIT_SOCIALIZE_PLUGIN', __FILE__);
 
 require_once __DIR__ . '/classes/autoload.php';
 
